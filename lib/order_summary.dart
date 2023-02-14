0ff4c0ab5919c7a4d68a8cfd2df3bb3751f66a05
NarutoUzumaki08 <20bcs241@iiitdmj.ac.in>
2023-02-14 15:30:11 +0530

    added more screens

--- lib/order_summary.dart
+++ lib/order_summary.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class OrderSummary extends StatelessWidget {
+  const OrderSummary({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
